74edcd26a5ab17a2129a03cda5b7a0e20914aa35
Yann Dubois <yanndubois96@gmail.com>
2019-02-08 19:47:02 +0000

    Add force dataset.

---
+++ ForceAttentionLocalization/generate_force_attn_loc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 ../AttentionLocalization/make_attn_loc.py -d . -n 5000 --target-weights --no-target-attention --not-add-rep --max-step 1 --max-len-src 15 --max-len-tgt 20
